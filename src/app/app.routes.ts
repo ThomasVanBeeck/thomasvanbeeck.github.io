8a36e989b252613c3fd06d68236172f6166f052d
CalTecks <thomas.van.beeck@pandora.be>
2025-09-30 01:15:23 +0200

    pages added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AboutmeComponent } from './pages/aboutme/aboutme.component';
+import { ReposComponent } from './pages/repos/repos.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AboutmeComponent },
+  { path: 'aboutme', component: AboutmeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'repos', component: ReposComponent},
+  { path: '**', redirectTo: '' }
+];
---
+++ src/app/pages/aboutme/aboutme.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-aboutme',
+  imports: [],
+  templateUrl: './aboutme.component.html',
+  styleUrl: './aboutme.component.css'
+})
+export class AboutmeComponent {
+
+}
